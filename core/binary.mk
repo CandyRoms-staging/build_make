3ae4406ab9b6fa53cb7be73cd9cedd7d6e4386e7
Chirayu Desai <chirayudesai1@gmail.com>
2021-01-30 19:19:01 +0530

    Replace old-style kernel headers only when building inline
    Change-Id: I71b891928a13771c176adc0eaa79b46503cb176f

--- core/binary.mk
+++ core/binary.mk
@@ -66,6 +66,12 @@ ifneq (,$(findstring $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr,$(LOCAL_ADDITION
 endif
 endif
 
+# Replace device_kernel_headers with generated_kernel_headers
+ifneq (,$(findstring device_kernel_headers,$(LOCAL_HEADER_LIBRARIES)))
+  LOCAL_HEADER_LIBRARIES := $(patsubst device_kernel_headers,generated_kernel_headers,$(LOCAL_HEADER_LIBRARIES))
+endif
+endif
+
 # The following LOCAL_ variables will be modified in this file.
 # Because the same LOCAL_ variables may be used to define modules for both 1st arch and 2nd arch,
 # we can't modify them in place.
